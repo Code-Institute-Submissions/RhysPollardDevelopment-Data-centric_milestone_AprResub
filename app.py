67b496482db04312633db48f5cdbe771fc2f5809
RhysPollardDevelopment <rpollard12@hotmail.co.uk>
2020-11-23 18:10:01 +0000

    Created function to send input data from form to database

--- app.py
+++ app.py
@@ -20,15 +20,42 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("base.html")
+    routes = list(mongo.db.routes.find())
+    return render_template("index.html", routes=routes)
 
 
-@app.route("/add_walk")
+@app.route("/add_walk",methods={"GET","POST"})
 def add_walk():
+    if request.method == "POST":
+        dogs_allowed = True if request.form.get("dogs_allowed") else False
+        free_parking = True if request.form.get("free_parking") else False
+        paid_parking = True if request.form.get("paid_parking") else False
+        walk = {
+            "category_name" : request.form.get("category_name"),
+            "title" : request.form.get("title"),
+            "description": request.form.get("description"),
+            # Splitlines function references from w3 schools at
+            # https://www.w3schools.com/python/ref_string_splitlines.asp
+            "directions" : request.form.get("directions").splitlines(),
+            "difficulty" : request.form.get("difficulty"),
+            "time" : request.form.get("time"),
+            "distance" : request.form.get("distance"),
+            "startpoint" : request.form.get("startpoint"),
+            "dogs_allowed" : dogs_allowed,
+            "free_parking" : free_parking,
+            "paid_parking" : paid_parking
+        }
+        mongo.db.routes.insert_one(walk)
+        return redirect(url_for("home"))
+
     categories = mongo.db.categories.find()
     return render_template("addwalk.html", categories=categories)
 
 
+
+@app.route("/show_route")
+def show_walk(_id):
+    return render_template("viewwalk.html", walk=mongo.db.routes.find_one({'_id':ObjectId(_id)}))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
