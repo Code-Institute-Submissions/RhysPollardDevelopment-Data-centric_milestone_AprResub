eab940990772949e18b01cba456bd54f020ce7aa
RhysPollardDevelopment <rpollard12@hotmail.co.uk>
2020-12-01 13:15:12 +0000

    Feature/Validation: Added custom code in Javascript to compare password and password confirmation in front-end and display a validation warning message.

---
+++ static/js/scripts.js
@@ -0,0 +1,22 @@
+let password = document.getElementById("password");
+let confirm_password = document.getElementById("confirm_password");
+
+// Keyup code refined using mozilla site
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/keyup_event
+if (password) {
+    password.addEventListener('keyup', validatePassword);
+    confirm_password.addEventListener('keyup', validatePassword);
+}
+
+// Checks that both Passwords are same after each character and sets the
+// warning for validity.
+// Idea for validate password found at https://codepen.io/diegoleme/pen/surIK
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/setCustomValidity
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords do not match!");
+    }
+    else {
+        confirm_password.setCustomValidity("");
+    }
+}
