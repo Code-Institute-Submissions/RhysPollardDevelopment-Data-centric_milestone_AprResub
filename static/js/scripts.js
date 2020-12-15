d47acf83a321d3f026d0d49b3c69b41c5aad1767
RhysPollardDevelopment <rpollard12@hotmail.co.uk>
2020-12-15 14:33:38 +0000

    Merge session-branch

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -52,3 +52,22 @@ function confirm_delete(recipe_delete_url){
       window.location.href = recipe_delete_url;
   }
 }
+
+
+/* Checks the state of a pages heart icon and updates user data
+ with route name as required. Advice found at:
+ https://stackoverflow.com/questions/37631388/how-to-get-data-in-flask-from-ajax-post*/
+let fav_icon = document.getElementById("favourite")
+fav_icon.addEventListener("change", function () {
+  url = window.location.pathname
+  url = url.replace("/show_route/", "")
+  /* need full version of ajax apparently */
+  $.ajax({
+    type:"POST",
+    url: '/toggle_favourite',
+    data: {
+      id: url,
+      checkbox: fav_icon.checked
+      }
+    })
+  })
