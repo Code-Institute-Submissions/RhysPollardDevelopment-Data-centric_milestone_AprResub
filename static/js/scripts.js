8a20daefeee4600bdf3327ab89ce3215ce5b8fb0
RhysPollardDevelopment <rpollard12@hotmail.co.uk>
2021-01-14 16:53:42 +0000

    Fix/Style: Justify-content-center used in userprofile to sit incomplete rows into center of the page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -25,10 +25,12 @@ $(document).ready(function () {
   const scrollArea = document.getElementById("favScroll");
   const favCount = document.getElementById("favNumber");
   // Removes buttons if less favourite items than overflow requires.
+  // Adds justify-content-center if no overflow.
   if (scrollArea) {
     if (scrollArea.clientWidth >= scrollArea.scrollWidth) {
       leftButton.style.visibility = "hidden";
       rightButton.style.visibility = "hidden";
+      scrollArea.classList.add("justify-content-center")
     } else {
       leftButton.onclick = function () {
         scrollArea.scrollLeft -= 320;
