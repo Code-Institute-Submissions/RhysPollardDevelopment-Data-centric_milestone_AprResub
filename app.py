706e372dd5ce05aeffae890c0952d595d15f9655
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-01-14 20:24:47 +0000

    Python: Added page titles to both error pages

--- app.py
+++ app.py
@@ -485,12 +485,14 @@ def privacy_policy():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html"), 404
+    page_title = "404 Page not found"
+    return render_template("404.html", page_title=page_title), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    return render_template("500.html"), 500
+    page_title = "500 Unexpected Error"
+    return render_template("500.html", page_title=page_title), 500
 
 
 if __name__ == "__main__":
