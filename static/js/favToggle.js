ec06de1edb01847e1ecdf5e8885c87972e2f3bfd
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-02-03 15:21:34 +0000

    Bug: Minor fixes to favtoggle file name for testing on heroku and some alterations to javascript from online code linter.

--- static/js/favToggle.js
+++ static/js/favToggle.js
@@ -7,21 +7,21 @@ $(document).ready(function () {
  https://stackoverflow.com/questions/37631388/how-to-get-data-in-flask-from-ajax-post*/
 
 function bindFavourite() {
-  let fav_icon = document.getElementById("favourite")
+  let fav_icon = document.getElementById("favourite");
   if (fav_icon) {
     fav_icon.addEventListener("change", function () {
-    url = window.location.pathname
-    url = url.replace("/show_route/", "")
-    /* need full version of ajax apparently */
-    $.ajax({
-      type:"POST",
-      url: '/toggle_favourite',
-      data: {
-        id: url,
-        checkbox: fav_icon.checked
+      let url = window.location.pathname;
+      url = url.replace("/show_route/", "");
+      /* need full version of ajax apparently */
+      $.ajax({
+        type: "POST",
+        url: '/toggle_favourite',
+        data: {
+          id: url,
+          checkbox: fav_icon.checked
         }
-      })
-    })
+      });
+    });
   }
 }
 
